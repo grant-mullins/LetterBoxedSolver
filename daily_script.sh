cd6a508577aeca29128148b889448b40199ef4e5
grant-mullins <grant0227@gmail.com>
2025-08-14 16:43:35 -0400

    Modified to run with crontab

--- daily_script.sh
+++ daily_script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-source .venv/bin/activate
-cd ./PastGames
-../lbs -w
+lbs_path=/home/grant/Documents/LetterBoxedSolver
+cd $lbs_path/PastGames
+
 date_str=$(date +%Y-%m-%d)
+../.venv/bin/python3 ../lbs -w $lbs_path/PastGames/$date_str.txt
 echo "$date_str Success"
 
